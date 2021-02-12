da06450a9bcbc2f7cfa34e0ce455b50c2c7e8751
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2021-02-12 16:11:49 +0300

    C# Study

---
+++ C#_Study/Print_I/CSharp.cs
@@ -0,0 +1,18 @@
+﻿//Dogukan Kaan Bozkurt
+
+using System;
+
+namespace FundamentalsOfCs
+{
+    class CSharp
+    {
+        static void Main()
+        {
+            //Basic print func.
+            Console.WriteLine("Hello World!");
+            //Waiting for a key to close cmd.
+            Console.ReadKey();
+            
+        }
+    }
+}
