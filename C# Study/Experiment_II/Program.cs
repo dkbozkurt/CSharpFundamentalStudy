65982159c5678f34b938e6fadde1f4729ff56cd2
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2021-02-13 00:14:35 +0300

    C#_Study

---
+++ C# Study/Experiment_II/Program.cs
@@ -0,0 +1,72 @@
+﻿//Dogukan Kaan Bozkurt
+
+using System;
+
+namespace Experiment_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string in_put= Console.ReadLine();   //Basic Input! Firstly it should attempted to string then we should transfrom.
+
+            //Kullanicidan alinan sayinin 2 fazlasini hesapla.
+
+
+            //int num = 0;
+            //Console.WriteLine("Hi! Please enter an integer values: ");
+            //string say = Console.ReadLine();
+            //num = Convert.ToInt32(say);
+            //Console.WriteLine("2 more than the input value is:");
+            //Console.WriteLine(num+2);
+            //Console.ReadKey(); 
+
+
+            //Kullanicidan alinan sayinin karesini hesaplama
+
+
+            //Console.WriteLine("Please enter a value:");
+            //string a = Console.ReadLine();
+            //float num = Convert.ToInt32(a);
+            //num *= num;
+            //Console.WriteLine("Square of " + a + " is:\n"+num);
+            //Console.ReadKey();
+
+
+            //Ekrandan girilen iki sayiyi toplama
+
+
+            //Console.WriteLine("Please enter two values for summation:");
+            //int s1, s2;
+            //string ints1=Console.ReadLine();
+            //string ints2 = Console.ReadLine();
+
+            //int sum = Convert.ToInt32(ints1) + Convert.ToInt32(ints2);
+            //Console.WriteLine(ints1+"+"+ints2+ " = " +sum);
+            //Console.ReadKey();
+
+
+            //Tasarruf hesaplayicisi, Kac ampulunuz var ve kac saat kullaniyorsunuz? 2 Tip ampul normal 2tl,tasarruflu %20 karli
+            //Normal ampul faturasi ve tasarruflu faturasi
+
+            //int bulbnum;
+            //double bulbhour;
+            //float nb=2, tb=1.6f;
+            //float bill;
+
+            //Console.WriteLine("Please enter total number of bulbs:");
+            //bulbnum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter total hours of using bulbs:");
+            //bulbhour = Convert.ToDouble(Console.ReadLine());
+            //bill = (float)(bulbnum * bulbhour * nb);
+            //Console.WriteLine("Total cost with normal bulbs is:\n" + bill + " $");
+            //bill = (float)(bulbnum * bulbhour * tb);
+            //Console.WriteLine("Total cost with economical bulbs is:\n" + bill + " $");
+            //Console.ReadKey();         
+            
+
+
+        }
+    }
+}
