a5762337f7d1dc4512c8d0cefe14f9d246ea2c03
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2021-02-15 00:20:34 +0300

    C#_Study

---
+++ C# Study/Encapsulation_VIII/Program.cs
@@ -0,0 +1,26 @@
+﻿//Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//ENCAPSULATE
+
+namespace Encapsulation_VIII
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rectangle rect = new Rectangle();
+            rect.a = -1;
+            rect.b = 5;
+
+
+            Console.WriteLine(rect.CalculateArea());
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ C# Study/Encapsulation_VIII/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿//Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_VIII
+{
+    public class Rectangle
+    {
+        private int _a;
+        private int _b;
+
+        //prop  ************ It is different than C++ so its important.
+
+        public int a
+        {
+            get { return _a; }
+            set
+            {
+                if (value < 0)      //By using this lines we pretend negative input values.
+                {
+                    _a = 0;
+                }
+                else
+                    _a = value;
+            }
+        }
+
+        public int b 
+        {
+            get { return _b; }
+            set
+            {
+                if (value < 0)
+                {
+                    _b = 0;
+                }
+                else
+                    _b = value;
+            }
+        }
+
+        public int CalculateArea()
+        {
+            return _a * _b;
+        }
+    }
+}
