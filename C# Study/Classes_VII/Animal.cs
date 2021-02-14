4c675ab715706011e7c0cca6e6c0437126a55778
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2021-02-14 23:16:11 +0300

    C#_Study

---
+++ C# Study/Classes_VII/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_VII
+{
+    public class Animal
+    {
+        public Animal()     //Constructor
+        {
+            name = "Not defined";
+            weight = -1;
+            gender = -1;
+        }
+        public int weight;
+        public string name;
+        public int gender;
+        
+        public string GetAnimalProperties()
+        {
+            //return "Weight: " + weight + "Name: " + name + "Gender: " + gender;       //Bunun yerine alttaki kullanilabilir.
+            return string.Format("Weight: {0} Name: {1} Gender: {2}",weight,name,gender);
+        }
+    }
+}
---
+++ C# Study/Classes_VII/Program.cs
@@ -0,0 +1,33 @@
+﻿//Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+
+
+//CLASSES
+
+namespace Classes_VII
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal cat = new Animal();
+            cat.name = "Micir";
+            cat.weight = 4;
+            cat.gender = 1;
+            Console.WriteLine(cat.GetAnimalProperties());
+
+            Animal bird = new Animal();
+            bird.name = "Kanarya";
+            bird.weight = 1;
+            bird.gender = 0;
+            Console.WriteLine(bird.GetAnimalProperties());
+
+            Animal dog = new Animal();
+            Console.WriteLine(dog.GetAnimalProperties());
+            Console.ReadLine();
+
+        }
+    }
+}
